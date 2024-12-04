9bea629be52069587b25cda8931729f1f6b26ce4
Kyle Mardell <kyleslufc@gmail.com>
2024-12-04 20:34:48 +0000

    Split post app code into the appropriate folders after accidentally writing in profiles. Reset migrations and apply new model migrations.

--- profiles/models.py
+++ profiles/models.py
@@ -13,10 +13,8 @@ class Profile(models.Model):
         upload_to='images/', default='https://res.cloudinary.com/dikde3vj3/image/upload/v<version>/default_profile_f0fbij.jpg'
     )
 
-
     class Meta:
         ordering = ['-created_at']
-
 
     def __str__(self):
         return f"{self.owner}'s profile"
@@ -29,44 +27,3 @@ def create_profile(sender, instance, created, **kwargs):
 
 post_save.connect(create_profile, sender=User)
 
-
-class Post(models.Model):
-    """
-    Post model, related to 'owner', i.e. a User instance.
-    Default image set so that we can always reference image.url.
-    """
-
-    image_filter_choices = [
-        ('_1977', '1977'),
-        ('brannan', 'Brannan'),
-        ('earlybird', 'Earlybird'),
-        ('hudson', 'Hudson'),
-        ('inkwell', 'Inkwell'),
-        ('lofi', 'Lo-Fi'),
-        ('kelvin', 'Kelvin'),
-        ('normal', 'Normal'),
-        ('nashville', 'Nashville'),
-        ('rise', 'Rise'),
-        ('toaster', 'Toaster'),
-        ('valencia', 'Valencia'),
-        ('walden', 'Walden'),
-        ('xpro2', 'X-pro II')
-    ]
-
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    image = models.ImageField(
-        upload_to='images/', default='../default_post_rgq6aq', blank=True
-    )
-    image_filter = models.CharField(
-        max_length=32, choices=image_filter_choices, default='normal'
-    )
-
-    class Meta:
-        ordering = ['-created_at']
-
-    def __str__(self):
-        return f'{self.id} {self.title}'
