643c24f435bc3b6a64f060b88e658b60658802f7
Kyle Mardell <kyleslufc@gmail.com>
2024-12-10 22:44:25 +0000

    Add filters to the profiles and posts apps.

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 from posts.models import Post
+from likes.models import Like
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
+    comments_count = serializers.ReadOnlyField()
+    likes_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -22,9 +26,21 @@ class PostSerializer(serializers.ModelSerializer):
             )
         return value
 
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, like=obj.owner
+            )
+            return like_id if like else None
+        return None
+
 
     class Meta:
         model = Post
@@ -32,4 +48,5 @@ class PostSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image', 'image_filter',
+            'like_id', 'comments_count', 'likes_count'
         ]
