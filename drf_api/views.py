f8189cc90c382c466f0cde644c16513ddb436d15
Kyle Mardell <kyleslufc@gmail.com>
2024-12-15 23:07:17 +0000

    Update views

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,9 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .settings import (
-    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
-    JWT_AUTH_SECURE,
-)
+from django.conf import settings  # Import settings instead of individual variables
 
 
 @api_view()
@@ -18,21 +15,21 @@ def root_route(request):
 def logout_route(request):
     response = Response()
     response.set_cookie(
-        key=JWT_AUTH_COOKIE,
+        key=settings.REST_AUTH['JWT_AUTH_COOKIE'],  # Access dictionary keys properly
         value='',
         httponly=True,
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
+        samesite=settings.REST_AUTH['JWT_AUTH_SAMESITE'],  # Same here
+        secure=getattr(settings, 'JWT_AUTH_SECURE', False),  # Handle `JWT_AUTH_SECURE` dynamically
     )
     response.set_cookie(
-        key=JWT_AUTH_REFRESH_COOKIE,
+        key=settings.REST_AUTH['JWT_AUTH_REFRESH_COOKIE'],  # Same fix
         value='',
         httponly=True,
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
+        samesite=settings.REST_AUTH['JWT_AUTH_SAMESITE'],
+        secure=getattr(settings, 'JWT_AUTH_SECURE', False),
     )
     return response
